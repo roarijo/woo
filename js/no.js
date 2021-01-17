29363de04efe86ee94382344181692103f712dc9
roarijo89 Tweaks <roarijo89@gmail.com>
2021-01-16 20:22:28 -0500

    Dont touch my shit bitch woo

---
+++ js/no.js
@@ -0,0 +1,9 @@
+function pop(){
+    let timerInterval
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Coco store Coming January 4 2021',
+      footer: '<a href="https://hub.cocotweaks.net">Visit the Coco hub for more</a>'
+    });
+  }
